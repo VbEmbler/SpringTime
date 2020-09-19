7583be7ceb2dedd30d7c54e30477ecb65c12e55b
Embler <vbembler@gmail.com>
2020-09-19 12:18:58 +0300

    Initial Commit

---
+++ SpringTime/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpringTime
+//
+//  Created by Vladimir on 19/09/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
